3b9b5ac778f64fc650419bc2b11885f836a9a2ec
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-13 01:38:53 +0900

    Add initial code for 06-16

---
+++ chapter06/exercise16/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ShowCursorPos.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ShowCursorPosTYPE           130
+#define ID_INDICATOR_POS                59142
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
