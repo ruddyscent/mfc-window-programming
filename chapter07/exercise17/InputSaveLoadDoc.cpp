64dd78008f2818ebfc3bb064337503681c5ef2e1
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-14 22:49:52 +0900

    Add base code for 07-17

---
+++ chapter07/exercise17/InputSaveLoadDoc.cpp
@@ -0,0 +1,172 @@
+﻿
+// InputSaveLoadDoc.cpp: CInputSaveLoadDoc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "InputSaveLoad.h"
+#endif
+
+#include "InputSaveLoadDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CInputSaveLoadDoc
+
+IMPLEMENT_DYNCREATE(CInputSaveLoadDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CInputSaveLoadDoc, CDocument)
+	ON_COMMAND(ID_STYLE_ITALIC, &CInputSaveLoadDoc::OnStyleItalic)
+	ON_UPDATE_COMMAND_UI(ID_STYLE_ITALIC, &CInputSaveLoadDoc::OnUpdateStyleItalic)
+	ON_COMMAND(ID_STYLE_UNDERLINE, &CInputSaveLoadDoc::OnStyleUnderline)
+	ON_UPDATE_COMMAND_UI(ID_STYLE_UNDERLINE, &CInputSaveLoadDoc::OnUpdateStyleUnderline)
+END_MESSAGE_MAP()
+
+
+// CInputSaveLoadDoc 생성/소멸
+
+CInputSaveLoadDoc::CInputSaveLoadDoc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+CInputSaveLoadDoc::~CInputSaveLoadDoc()
+{
+}
+
+BOOL CInputSaveLoadDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	m_str.RemoveAll();
+	m_bItalic = m_bUnderline = FALSE;
+
+	return TRUE;
+}
+
+
+
+
+// CInputSaveLoadDoc serialization
+
+void CInputSaveLoadDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		ar << m_bItalic << m_bUnderline;
+		m_str.Serialize(ar);
+	}
+	else
+	{
+		ar >> m_bItalic >> m_bUnderline;
+		m_str.Serialize(ar);
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CInputSaveLoadDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT)GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CInputSaveLoadDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CInputSaveLoadDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl* pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CInputSaveLoadDoc 진단
+
+#ifdef _DEBUG
+void CInputSaveLoadDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CInputSaveLoadDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CInputSaveLoadDoc 명령
+
+
+void CInputSaveLoadDoc::OnStyleItalic()
+{
+	m_bItalic = !m_bItalic;
+	SetModifiedFlag();
+	UpdateAllViews(NULL); // 뷰의 화면을 갱신한다.
+}
+
+
+void CInputSaveLoadDoc::OnUpdateStyleItalic(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetCheck(m_bItalic == TRUE);
+}
+
+
+void CInputSaveLoadDoc::OnStyleUnderline()
+{
+	m_bUnderline = !m_bUnderline;
+	SetModifiedFlag();
+	UpdateAllViews(NULL); // 뷰의 화면을 갱신한다.
+}
+
+
+void CInputSaveLoadDoc::OnUpdateStyleUnderline(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetCheck(m_bUnderline == TRUE);
+}
---
+++ chapter07/exercise17/InputSaveLoadView.cpp
@@ -0,0 +1,138 @@
+﻿
+// InputSaveLoadView.cpp: CInputSaveLoadView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "InputSaveLoad.h"
+#endif
+
+#include "InputSaveLoadDoc.h"
+#include "InputSaveLoadView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CInputSaveLoadView
+
+IMPLEMENT_DYNCREATE(CInputSaveLoadView, CView)
+
+BEGIN_MESSAGE_MAP(CInputSaveLoadView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_CHAR()
+END_MESSAGE_MAP()
+
+// CInputSaveLoadView 생성/소멸
+
+CInputSaveLoadView::CInputSaveLoadView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CInputSaveLoadView::~CInputSaveLoadView()
+{
+}
+
+BOOL CInputSaveLoadView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CInputSaveLoadView 그리기
+
+void CInputSaveLoadView::OnDraw(CDC* pDC)
+{
+	CInputSaveLoadDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// 화면 출력용 폰트를 선택한다.
+	CFont font;
+	font.CreateFont(30, 0, 0, 0, 0, pDoc->m_bItalic,
+		pDoc->m_bUnderline, 0, 0, 0, 0, 0, 0, _T("궁서"));
+	pDC->SelectObject(&font);
+
+	// 현재까지 입력된 글자를 화면에 출력한다.
+	CRect rect;
+	GetClientRect(&rect);
+	pDC->DrawText(pDoc->m_str.GetData(),
+		pDoc->m_str.GetSize(), &rect, DT_LEFT);
+}
+
+
+// CInputSaveLoadView 인쇄
+
+BOOL CInputSaveLoadView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CInputSaveLoadView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CInputSaveLoadView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CInputSaveLoadView 진단
+
+#ifdef _DEBUG
+void CInputSaveLoadView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CInputSaveLoadView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CInputSaveLoadDoc* CInputSaveLoadView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CInputSaveLoadDoc)));
+	return (CInputSaveLoadDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CInputSaveLoadView 메시지 처리기
+
+
+void CInputSaveLoadView::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	CInputSaveLoadDoc* pDoc = GetDocument();
+
+	// [Backspace] 입력 시 맨 마지막 글자를 삭제한다.
+	if (nChar == _T('\b')) {
+		if (pDoc->m_str.GetSize() > 0)
+			pDoc->m_str.RemoveAt(pDoc->m_str.GetSize() - 1);
+	}
+	// 그 밖의 경우에는 동적 배열에 글자를 추가한다.
+	else {
+		pDoc->m_str.Add(nChar);
+	}
+
+	// 데이터가 수정되었음을 도큐먼트 객체에 알린다.
+	pDoc->SetModifiedFlag();
+
+	// 뷰의 화면을 갱신한다.
+	Invalidate();
+}
