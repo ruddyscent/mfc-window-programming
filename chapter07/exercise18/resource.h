9f9fe1ef8f1155ee93681177062165b99583b3e2
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-14 23:15:18 +0900

    Add base code for 07-18

---
+++ chapter07/exercise18/resource.h
@@ -0,0 +1,20 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// InputSaveLoad.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_InputSaveLoadTYPE           130
+#define ID_STYLE_ITALIC                 32771
+#define ID_STYLE_UNDERLINE              32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
