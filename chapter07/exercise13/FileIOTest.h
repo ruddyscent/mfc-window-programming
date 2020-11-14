48ad13d02f1d856c32927d86d35f95c0dd54ad5a
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-14 17:07:45 +0900

    Add base code for 07-13

---
+++ chapter07/exercise13/FileIOTest.h
@@ -0,0 +1,32 @@
+﻿
+// FileIOTest.h: FileIOTest 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CFileIOTestApp:
+// 이 클래스의 구현에 대해서는 FileIOTest.cpp을(를) 참조하세요.
+//
+
+class CFileIOTestApp : public CWinApp
+{
+public:
+	CFileIOTestApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFileIOTestApp theApp;
