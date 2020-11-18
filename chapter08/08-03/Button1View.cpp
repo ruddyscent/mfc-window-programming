35d1f31f792ac5be9a4eccc57a4008841f8946e4
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-18 22:08:26 +0900

    Solve 08-03

--- chapter08/08-03/Button1View.cpp
+++ chapter08/08-03/Button1View.cpp
@@ -29,6 +29,7 @@ BEGIN_MESSAGE_MAP(CButton1View, CView)
 	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
 	ON_WM_CREATE()
 	ON_BN_CLICKED(101, OnButtonClicked)
+	ON_BN_CLICKED(104, OnRadioButtonClicked)
 END_MESSAGE_MAP()
 
 // CButton1View 생성/소멸
@@ -119,7 +120,7 @@ int CButton1View::OnCreate(LPCREATESTRUCT lpCreateStruct)
 		CRect(20, 60, 160, 90), this, 102);
 	m_3state.Create(_T("3상태 체크 박스"), WS_CHILD | WS_VISIBLE | BS_AUTO3STATE,
 		CRect(20, 100, 160, 130), this, 103);
-	m_radio1.Create(_T("라디오 버튼 1"), WS_CHILD | WS_VISIBLE | WS_GROUP | BS_AUTORADIOBUTTON,
+	m_radio1.Create(_T("라디오 버튼 1"), WS_CHILD | WS_VISIBLE | WS_GROUP | BS_RADIOBUTTON,
 		CRect(20, 170, 160, 200), this, 104);
 	m_radio2.Create(_T("라디오 버튼 2"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
 		CRect(20, 210, 160, 240), this, 105);
@@ -146,4 +147,18 @@ void CButton1View::OnButtonClicked()
 	str.Format(_T("버튼 상태: %d, %d, %d, %d"),
 		state_checkbox, state_3state, state_radio1, state_radio2);
 	MessageBox(str, _T("Button1 예제"), MB_ICONINFORMATION);
+}
+
+
+void CButton1View::OnRadioButtonClicked()
+{
+	// TODO: 여기에 구현 코드 추가.
+	if (m_radio1.GetCheck() == BST_CHECKED) {
+		m_radio1.SetCheck(BST_UNCHECKED);
+		m_radio2.SetCheck(BST_CHECKED);
+	}
+	else {
+		m_radio1.SetCheck(BST_CHECKED);
+		m_radio2.SetCheck(BST_UNCHECKED);
+	}
 }
