5c30996ce049e1deee5bb527ea880f4291edbaa6
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-21 20:29:09 +0900

    Add base codes for 08-08

---
+++ chapter08/08-08/StaticView.h
@@ -0,0 +1,59 @@
+﻿
+// StaticView.h: CStaticView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CStaticView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CStaticView() noexcept;
+	DECLARE_DYNCREATE(CStaticView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_STATIC_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CStaticDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CStaticView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_metafile;
+	afx_msg void OnStnClickedMetafile();
+};
+
+#ifndef _DEBUG  // StaticView.cpp의 디버그 버전
+inline CStaticDoc* CStaticView::GetDocument() const
+   { return reinterpret_cast<CStaticDoc*>(m_pDocument); }
+#endif
+
---
+++ chapter08/08-08/resource.h
@@ -0,0 +1,20 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Static.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDD_STATIC_FORM                 101
+#define IDR_MAINFRAME                   128
+#define IDR_StaticTYPE                  130
+#define IDC_METAFILE                    1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
