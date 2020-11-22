66e09f9baf748a52cd30c72d88b73047854b3b1c
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-22 19:18:23 +0900

    Add base codes for 08-11

---
+++ chapter08/08-11/EditCtrlView.cpp
@@ -0,0 +1,130 @@
+﻿
+// EditCtrlView.cpp: CEditCtrlView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "EditCtrl.h"
+#endif
+
+#include "EditCtrlDoc.h"
+#include "EditCtrlView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CEditCtrlView
+
+IMPLEMENT_DYNCREATE(CEditCtrlView, CFormView)
+
+BEGIN_MESSAGE_MAP(CEditCtrlView, CFormView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CFormView::OnFilePrintPreview)
+	ON_EN_CHANGE(IDC_MYEDIT, &CEditCtrlView::OnEnChangeMyedit)
+	ON_EN_MAXTEXT(IDC_MYEDIT, &CEditCtrlView::OnEnMaxtextMyedit)
+END_MESSAGE_MAP()
+
+// CEditCtrlView 생성/소멸
+
+CEditCtrlView::CEditCtrlView() noexcept
+	: CFormView(IDD_EDITCTRL_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CEditCtrlView::~CEditCtrlView()
+{
+}
+
+void CEditCtrlView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_MYEDIT, m_edit);
+	DDX_Control(pDX, IDC_MYSTATIC, m_static);
+}
+
+BOOL CEditCtrlView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CEditCtrlView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+	m_edit.SetLimitText(20);
+}
+
+
+// CEditCtrlView 인쇄
+
+BOOL CEditCtrlView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CEditCtrlView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CEditCtrlView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CEditCtrlView::OnPrint(CDC* pDC, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 여기에 사용자 지정 인쇄 코드를 추가합니다.
+}
+
+
+// CEditCtrlView 진단
+
+#ifdef _DEBUG
+void CEditCtrlView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CEditCtrlView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CEditCtrlDoc* CEditCtrlView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CEditCtrlDoc)));
+	return (CEditCtrlDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CEditCtrlView 메시지 처리기
+
+
+void CEditCtrlView::OnEnChangeMyedit()
+{
+	CString str;
+	m_edit.GetWindowText(str);
+	m_static.SetWindowText(str);
+}
+
+
+void CEditCtrlView::OnEnMaxtextMyedit()
+{
+	MessageBox(_T("최대 길이 도달!"), _T("오류"), MB_ICONERROR);
+}
