6642f70b48c547810e8a7a19e1f6ad8fda7f2def
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-25 02:34:09 +0900

    Add base codes for 08-16

---
+++ chapter08/08-16/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ListBox.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDD_LISTBOX_FORM                101
+#define IDR_MAINFRAME                   128
+#define IDR_ListBoxTYPE                 130
+#define IDC_LIST1                       1000
+#define IDC_LIST2                       1001
+#define IDC_RIGHT                       1002
+#define IDC_LEFT2                       1003
+#define IDC_LEFT                        1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
