4669f8b5bc58c8c7fc8f404615e12cbfa94471c0
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-25 01:54:36 +0900

    Add base codes for 03-17

---
+++ chapter03/03-17/MainFrm.h
@@ -0,0 +1,45 @@
+﻿
+// MainFrm.h: CMainFrame 클래스의 인터페이스
+//
+
+#pragma once
+#include "ChildView.h"
+
+class CMainFrame : public CFrameWnd
+{
+	
+public:
+	CMainFrame() noexcept;
+protected: 
+	DECLARE_DYNAMIC(CMainFrame)
+
+// 특성입니다.
+public:
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+
+// 구현입니다.
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+	CChildView    m_wndView;
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
+	DECLARE_MESSAGE_MAP()
+
+};
+
+
