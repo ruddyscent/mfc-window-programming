a3d978b03d25cac549d09a7e09c93d661dbd9293
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-24 23:46:08 +0900

    Solve 03-02

--- chapter03/03-02/ChildView.cpp
+++ chapter03/03-02/ChildView.cpp
@@ -39,7 +39,7 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 	cs.dwExStyle |= WS_EX_CLIENTEDGE;
 	cs.style &= ~WS_BORDER;
 	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
-		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+		::LoadCursor(nullptr, IDC_HAND), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
 
 	return TRUE;
 }
