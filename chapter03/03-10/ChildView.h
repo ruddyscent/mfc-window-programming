689c9cba133b20e33b922c4b5543f698525be0d3
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-25 00:51:31 +0900

    Add base codes for 03-10

---
+++ chapter03/03-10/ChildView.h
@@ -0,0 +1,36 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+// CChildView 창
+
+class CChildView : public CWnd
+{
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+};
+
