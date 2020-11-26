2746f24e9099c3060a50a3856978c636bcfab92f
Kyungwon Chun <ruddyscent@gmail.com>
2020-11-26 20:36:30 +0900

    Add base codes for 04-06

---
+++ chapter04/04-06/ChildView.cpp
@@ -0,0 +1,82 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MetaFileDC.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	// 확장 메타파일 객체를 생성하고 초기화한다.
+	CMetaFileDC dc;
+	dc.CreateEnhanced(NULL, NULL, NULL, NULL);
+	// 멤버 함수를 호출하여 출력한다.
+	dc.Rectangle(0, 0, 10, 10);
+	dc.Ellipse(1, 1, 9, 9);
+	// 확장 메타파일 핸들을 얻는다.
+	m_hmf = dc.CloseEnhanced();
+}
+
+CChildView::~CChildView()
+{
+	// 확장 메타파일을 삭제한다.
+	::DeleteEnhMetaFile(m_hmf);
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+	CRect rect(point.x - 20, point.y - 20, point.x + 20, point.y + 20);
+	dc.PlayMetaFile(m_hmf, &rect);
+}
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+	CRect rect(point.x - 50, point.y - 20, point.x + 50, point.y + 20);
+	dc.PlayMetaFile(m_hmf, &rect);
+}
---
+++ chapter04/04-06/MetaFileDC.h
@@ -0,0 +1,35 @@
+﻿
+// MetaFileDC.h: MetaFileDC 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMetaFileDCApp:
+// 이 클래스의 구현에 대해서는 MetaFileDC.cpp을(를) 참조하세요.
+//
+
+class CMetaFileDCApp : public CWinApp
+{
+public:
+	CMetaFileDCApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMetaFileDCApp theApp;
