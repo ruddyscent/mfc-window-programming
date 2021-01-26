5d16e4e62b6b57d4dac0b76184b410784fb9075f
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 03:00:34 +0900

    Solve 04-17

--- chapter04/04-17/ChildView.cpp
+++ chapter04/04-17/ChildView.cpp
@@ -16,6 +16,7 @@
 
 CChildView::CChildView()
 {
+	nBkMode = TRANSPARENT;
 }
 
 CChildView::~CChildView()
@@ -25,6 +26,8 @@ CChildView::~CChildView()
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
 END_MESSAGE_MAP()
 
 
@@ -47,14 +50,36 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 void CChildView::OnPaint()
 {
 	CPaintDC dc(this);
-	dc.SetBkMode(OPAQUE); // 이 줄을 주석 처리해도 결과는 같음
+	dc.SetBkMode(nBkMode);
 	dc.TextOut(100, 50, CString(" OPAQUE 모드 [1] "));
 
-	dc.SetBkMode(TRANSPARENT);
 	dc.TextOut(100, 100, CString(" TRANSPARENT 모드 "));
 
-	dc.SetBkMode(OPAQUE);
 	dc.SetBkColor(RGB(0, 255, 0)); // 배경을 초록색으로 설정
 	dc.TextOut(100, 150, CString(" OPAQUE 모드 [2] "));
 }
 
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CWnd::OnLButtonDown(nFlags, point);
+
+	CClientDC dc(this);
+	nBkMode = OPAQUE;
+	CWnd::Invalidate();
+}
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CWnd::OnRButtonDown(nFlags, point);
+
+	CClientDC dc(this);
+	nBkMode = TRANSPARENT;
+	CWnd::Invalidate();
+}
