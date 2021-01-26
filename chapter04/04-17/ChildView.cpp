71e8e4dcb938d08dd62914f32839309d4fb289cc
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 02:42:44 +0900

    Add base codes for 04-17

---
+++ chapter04/04-17/ChildView.cpp
@@ -0,0 +1,60 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "BackgroundMode.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), (HBRUSH)GetStockObject(GRAY_BRUSH), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+	dc.SetBkMode(OPAQUE); // 이 줄을 주석 처리해도 결과는 같음
+	dc.TextOut(100, 50, CString(" OPAQUE 모드 [1] "));
+
+	dc.SetBkMode(TRANSPARENT);
+	dc.TextOut(100, 100, CString(" TRANSPARENT 모드 "));
+
+	dc.SetBkMode(OPAQUE);
+	dc.SetBkColor(RGB(0, 255, 0)); // 배경을 초록색으로 설정
+	dc.TextOut(100, 150, CString(" OPAQUE 모드 [2] "));
+}
+
