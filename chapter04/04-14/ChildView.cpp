e4bc930454c40b7a00d6c30dc5fdf0bef91a48bf
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 02:16:25 +0900

    Solve 04-14

--- chapter04/04-14/ChildView.cpp
+++ chapter04/04-14/ChildView.cpp
@@ -51,15 +51,68 @@ void CChildView::OnPaint()
 	CRect rect;
 	GetClientRect(&rect);
 
+	CFont font1, font2, font3;
+
+	VERIFY(font1.CreateFont(
+		24,                       // nHeight
+		0,                        // nWidth
+		0,                        // nEscapement
+		0,                        // nOrientation
+		FW_BOLD,                  // nWeight
+		FALSE,                    // bItalic
+		FALSE,                    // bUnderline
+		0,                        // cStrikeOut
+		ANSI_CHARSET,             // nCharSet
+		OUT_DEFAULT_PRECIS,       // nOutPrecision
+		CLIP_DEFAULT_PRECIS,      // nClipPrecision
+		DEFAULT_QUALITY,          // nQuality
+		DEFAULT_PITCH | FF_SWISS, // nPitchAndFamily
+		_T("Arial")));            // lpszFacename
+
+	VERIFY(font2.CreateFont(
+		24,                       // nHeight
+		0,                        // nWidth
+		0,                        // nEscapement
+		0,                        // nOrientation
+		FW_NORMAL,                // nWeight
+		TRUE,                     // bItalic
+		FALSE,                    // bUnderline
+		0,                        // cStrikeOut
+		ANSI_CHARSET,             // nCharSet
+		OUT_DEFAULT_PRECIS,       // nOutPrecision
+		CLIP_DEFAULT_PRECIS,      // nClipPrecision
+		DEFAULT_QUALITY,          // nQuality
+		DEFAULT_PITCH | FF_SWISS, // nPitchAndFamily
+		_T("Arial")));            // lpszFacename
+
+	VERIFY(font3.CreateFont(
+		24,                       // nHeight
+		0,                        // nWidth
+		0,                        // nEscapement
+		0,                        // nOrientation
+		FW_NORMAL,                // nWeight
+		FALSE,                    // bItalic
+		TRUE,                     // bUnderline
+		0,                        // cStrikeOut
+		ANSI_CHARSET,             // nCharSet
+		OUT_DEFAULT_PRECIS,       // nOutPrecision
+		CLIP_DEFAULT_PRECIS,      // nClipPrecision
+		DEFAULT_QUALITY,          // nQuality
+		DEFAULT_PITCH | FF_SWISS, // nPitchAndFamily
+		_T("Arial")));            // lpszFacename
+
 	dc.SetTextColor(RGB(255, 0, 0)); // 글자를 빨간색으로 설정
 	dc.SetBkColor(RGB(255, 255, 0)); // 배경을 노란색으로 설정
+	dc.SelectObject(font1);
 	dc.DrawText(CString("DrawText 함수를 연습합니다. [1]"), &rect, 0);
+	dc.SelectObject(font2);
 	dc.DrawText(CString("DrawText 함수를 연습합니다. [2]"), &rect,
 		DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
 	dc.SetTextAlign(TA_CENTER); // 가운데 정렬 방식으로 변경
 	dc.SetTextColor(RGB(0, 0, 255)); // 글자를 파란색으로 설정
 	dc.SetBkColor(RGB(0, 255, 0)); // 배경을 초록색으로 설정
+	dc.SelectObject(font3);
 	dc.TextOut(rect.right / 2, 3 * rect.bottom / 4,
 		CString("TextOut 함수를 연습합니다."));
 }
