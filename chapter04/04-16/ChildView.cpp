494137b76c4f975d921af19796078700ea614e9b
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 02:24:28 +0900

    Add base codes for 04-16

---
+++ chapter04/04-16/ChildView.cpp
@@ -0,0 +1,59 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MappingMode2.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+	CRect rect;
+	GetClientRect(&rect);
+	dc.SetMapMode(MM_ANISOTROPIC);
+	dc.SetWindowExt(100, 100);
+	dc.SetViewportExt(rect.Width(), rect.Height());
+	dc.RoundRect(0, 0, 100, 100, 50, 50);
+	dc.DrawEdge(CRect(20, 20, 80, 80),
+		BDR_SUNKENINNER | BDR_RAISEDOUTER, BF_RECT);
+}
+
---
+++ chapter04/04-16/MappingMode2.h
@@ -0,0 +1,35 @@
+﻿
+// MappingMode2.h: MappingMode2 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMappingMode2App:
+// 이 클래스의 구현에 대해서는 MappingMode2.cpp을(를) 참조하세요.
+//
+
+class CMappingMode2App : public CWinApp
+{
+public:
+	CMappingMode2App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMappingMode2App theApp;
