4d426a05e31179fddaa1585311cebf16cb16d4e6
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 23:13:36 +0900

    Add base codes for 04-22

---
+++ chapter04/04-22/Bitmaps.h
@@ -0,0 +1,35 @@
+﻿
+// Bitmaps.h: Bitmaps 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CBitmapsApp:
+// 이 클래스의 구현에 대해서는 Bitmaps.cpp을(를) 참조하세요.
+//
+
+class CBitmapsApp : public CWinApp
+{
+public:
+	CBitmapsApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBitmapsApp theApp;
