c42aef8a9579713edd139713d53e456b9b1c1fb1
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-28 00:25:42 +0900

    Add base codes for 04-24

---
+++ chapter04/04-24/ChildView.cpp
@@ -0,0 +1,58 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Regions.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+	CRgn rgn1, rgn2, rgn3;
+	rgn1.CreateRectRgn(0, 0, 1, 1); // 임의로 만든 리전
+	rgn2.CreateRectRgn(10, 10, 100, 100);
+	rgn3.CreateEllipticRgn(50, 50, 200, 200);
+	rgn1.CombineRgn(&rgn2, &rgn3, RGN_XOR);
+	CBrush brush(RGB(0, 0, 255));
+	dc.FillRgn(&rgn1, &brush);
+}
+
