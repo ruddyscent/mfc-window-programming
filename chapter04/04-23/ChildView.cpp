197f44ef73ae4f857d19a727bcbb1233214e237d
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-27 23:30:06 +0900

    Add base codes for 04-23

---
+++ chapter04/04-23/ChildView.cpp
@@ -0,0 +1,75 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Bitmaps.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+
+	// 비트맵 리소스를 로드한 후 크기 정보를 얻는다.
+	CBitmap bitmap;
+	bitmap.LoadBitmap(IDB_BITMAP1);
+	BITMAP bmpinfo;
+	bitmap.GetBitmap(&bmpinfo);
+
+	// 메모리 DC를 만든 후 비트맵을 선택해 넣는다.
+	CDC dcmem;
+	dcmem.CreateCompatibleDC(&dc);
+	dcmem.SelectObject(&bitmap);
+
+	// 비트맵을 화면에 출력한다.
+	dc.BitBlt(10, 10, bmpinfo.bmWidth, bmpinfo.bmHeight,
+		&dcmem, 0, 0, SRCCOPY);
+	dc.StretchBlt(10, 100, bmpinfo.bmWidth * 4, bmpinfo.bmHeight * 2,
+		&dcmem, 0, 0, bmpinfo.bmWidth, bmpinfo.bmHeight, SRCCOPY);
+
+	// 메모리 DC에 그림을 그린 후 다시 화면에 출력한다.
+	dcmem.Rectangle(5, 5, 15, 15);
+	dc.BitBlt(350, 10, bmpinfo.bmWidth, bmpinfo.bmHeight,
+		&dcmem, 0, 0, SRCCOPY);
+	dc.StretchBlt(350, 100, bmpinfo.bmWidth * 4, bmpinfo.bmHeight * 2,
+		&dcmem, 0, 0, bmpinfo.bmWidth, bmpinfo.bmHeight, SRCCOPY);
+}
+
---
+++ chapter04/04-23/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Bitmaps.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_BitmapsTYPE                 130
+#define IDB_BITMAP1                     310
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
