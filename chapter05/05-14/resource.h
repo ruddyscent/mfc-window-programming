cb5f0052c49395361e2737897f1723f6b0179d39
Kyungwon Chun <ruddyscent@gmail.com>
2021-01-30 18:34:19 +0900

    Add base codes for 05-14

---
+++ chapter05/05-14/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ChangeCursor.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ChangeCursorTYPE            130
+#define IDC_CURSOR1                     310
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
