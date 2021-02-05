c266ef4faf1efa4082ff201f28f597a40cdb4c08
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-05 09:28:15 +0900

    Add base codes for 05-08

---
+++ chapter05/05-08/ChildView.h
@@ -0,0 +1,42 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+// CChildView 창
+
+class CChildView : public CWnd
+{
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+	BOOL m_bDrawMode; // 그리기 작업이 진행 중임을 나타낸다.
+	int m_x1, m_y1, m_x2, m_y2; // 타원에 외접하는 직사각형의 좌상단/우하단 좌표
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
+
