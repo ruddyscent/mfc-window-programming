91b4f2d1a138a3032a2aa244ad5a4e5238a16354
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-06 23:20:23 +0900

    Add base codes for 05-21

---
+++ chapter05/05-21/ChildView.cpp
@@ -0,0 +1,89 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "InputKeyStroke.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_xPos = m_yPos = 60; // 임의 값으로 초기화
+	m_bFill = FALSE; // 도형 내부를 채우지 않음
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_SIZE()
+	ON_WM_KEYDOWN()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+	if (m_bFill == TRUE) dc.SelectStockObject(BLACK_BRUSH);
+	dc.Ellipse(m_xPos - 20, m_yPos - 20, m_xPos + 20, m_yPos + 20);
+}
+
+
+void CChildView::OnSize(UINT nType, int cx, int cy)
+{
+	m_xMax = cx;
+	m_yMax = cy;
+}
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	switch (nChar) {
+	case VK_LEFT:
+		m_xPos -= 20;
+		break;
+	case VK_RIGHT:
+		m_xPos += 20;
+		break;
+	case VK_UP:
+		m_yPos -= 20;
+		break;
+	case VK_DOWN:
+		m_yPos += 20;
+		break;
+	case VK_SPACE:
+		m_bFill = !m_bFill;
+	}
+	// 20 <= m_xPos <= m_xMax-20
+	m_xPos = min(max(20, m_xPos), m_xMax - 20);
+	// 20 <= m_yPos <= m_yMax-20
+	m_yPos = min(max(20, m_yPos), m_yMax - 20);
+	Invalidate();
+}
---
+++ chapter05/05-21/InputKeyStroke.h
@@ -0,0 +1,35 @@
+﻿
+// InputKeyStroke.h: InputKeyStroke 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CInputKeyStrokeApp:
+// 이 클래스의 구현에 대해서는 InputKeyStroke.cpp을(를) 참조하세요.
+//
+
+class CInputKeyStrokeApp : public CWinApp
+{
+public:
+	CInputKeyStrokeApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CInputKeyStrokeApp theApp;
