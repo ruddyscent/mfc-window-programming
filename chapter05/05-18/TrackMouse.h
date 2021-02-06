0b3643ad6053b09903ad5a18afa1a95693cf1336
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-06 15:36:30 +0900

    Add base codes for 05-18

---
+++ chapter05/05-18/TrackMouse.h
@@ -0,0 +1,35 @@
+﻿
+// TrackMouse.h: TrackMouse 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CTrackMouseApp:
+// 이 클래스의 구현에 대해서는 TrackMouse.cpp을(를) 참조하세요.
+//
+
+class CTrackMouseApp : public CWinApp
+{
+public:
+	CTrackMouseApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTrackMouseApp theApp;
