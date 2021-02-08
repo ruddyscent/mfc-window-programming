858490d2f44dabddef024e39bc988468873b6641
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-08 14:46:54 +0900

    Solve 06-01

--- chapter06/06-01/resource.h
+++ chapter06/06-01/resource.h
@@ -22,13 +22,14 @@
 #define ID_MATHFUNC_SIN                 32785
 #define ID_MATHFUNC_COS                 32786
 #define ID_MATHFUNC_TAN                 32787
+#define ID_Menu                         32788
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32788
+#define _APS_NEXT_COMMAND_VALUE         32789
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
