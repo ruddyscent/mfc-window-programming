902828c70539ccc91c7d2f3e4a0ac7e2727b723c
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-10 21:17:51 +0900

    Solve 06-05

--- chapter06/06-05/ChildView.cpp
+++ chapter06/06-05/ChildView.cpp
@@ -16,6 +16,12 @@
 
 CChildView::CChildView()
 {
+	int x1, y1, x2, y2;
+	x1 = 100;
+	y1 = 50;
+	x2 = 500;
+	y2 = 200;
+	m_rect.SetRect(x1, y1, x2, y2);
 }
 
 CChildView::~CChildView()
@@ -25,6 +31,7 @@ CChildView::~CChildView()
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
+	ON_WM_RBUTTONDOWN()
 END_MESSAGE_MAP()
 
 
@@ -49,7 +56,30 @@ void CChildView::OnPaint()
 	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 	
 	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
-	
+	dc.Rectangle(m_rect);
+
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
 
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	CMenu popup;
+	popup.CreatePopupMenu();
+	if (m_rect.PtInRect(point)) {
+		popup.AppendMenu(MF_STRING, 301, _T("삭제"));
+	}
+	else {
+		popup.AppendMenu(MF_STRING, 302, _T("추가"));
+	}
+	CRect rect;
+	GetClientRect(rect);
+	ClientToScreen(rect);
+	popup.TrackPopupMenu(
+		TPM_LEFTALIGN | TPM_RIGHTBUTTON,
+		point.x + rect.left, point.y + rect.top, AfxGetMainWnd());
+
+	CWnd::OnRButtonDown(nFlags, point);
+}
--- chapter06/06-05/ChildView.h
+++ chapter06/06-05/ChildView.h
@@ -16,6 +16,7 @@ public:
 
 // 특성입니다.
 public:
+	CRect m_rect;
 
 // 작업입니다.
 public:
@@ -32,5 +33,7 @@ public:
 protected:
 	afx_msg void OnPaint();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
 };
 
