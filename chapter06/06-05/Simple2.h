ef50d90dd8f026c1cd03a6ae91759fc82a45a288
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-10 19:50:56 +0900

    Add base codes for 06-05

---
+++ chapter06/06-05/Simple2.h
@@ -0,0 +1,35 @@
+﻿
+// Simple2.h: Simple2 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CSimple2App:
+// 이 클래스의 구현에 대해서는 Simple2.cpp을(를) 참조하세요.
+//
+
+class CSimple2App : public CWinApp
+{
+public:
+	CSimple2App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSimple2App theApp;
