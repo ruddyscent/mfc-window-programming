bbe8fb30ffd8b04be1a86bfab919ef12ce14406e
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-11 16:17:19 +0900

    Add base codes for 06-08

---
+++ chapter06/ColorSelect - 1. 명령 갱신 핸들러 추가/ChildView.cpp
@@ -0,0 +1,106 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ColorSelect.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_color = RGB(255, 0, 0);
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_COLOR_RED, &CChildView::OnColorRed)
+	ON_COMMAND(ID_COLOR_GREEN, &CChildView::OnColorGreen)
+	ON_COMMAND(ID_COLOR_BLUE, &CChildView::OnColorBlue)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_RED, &CChildView::OnUpdateColorRed)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_GREEN, &CChildView::OnUpdateColorGreen)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_BLUE, &CChildView::OnUpdateColorBlue)
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS,
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint()
+{
+	CPaintDC dc(this);
+
+	CFont font;
+	font.CreatePointFont(300, _T("궁서"));
+	dc.SelectObject(&font);
+	dc.SetTextColor(m_color);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CString str = _T("메뉴 테스트");
+	dc.DrawText(str, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+}
+
+
+void CChildView::OnColorRed()
+{
+	m_color = RGB(255, 0, 0);
+	Invalidate();
+}
+
+
+void CChildView::OnColorGreen()
+{
+	m_color = RGB(0, 255, 0);
+	Invalidate();
+}
+
+
+void CChildView::OnColorBlue()
+{
+	m_color = RGB(0, 0, 255);
+	Invalidate();
+}
+
+
+void CChildView::OnUpdateColorRed(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetCheck(m_color == RGB(255, 0, 0));
+}
+
+
+void CChildView::OnUpdateColorGreen(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetCheck(m_color == RGB(0, 255, 0));
+}
+
+
+void CChildView::OnUpdateColorBlue(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetCheck(m_color == RGB(0, 0, 255));
+}
---
+++ chapter06/ColorSelect - 1. 명령 갱신 핸들러 추가/ChildView.h
@@ -0,0 +1,44 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+// CChildView 창
+
+class CChildView : public CWnd
+{
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+	COLORREF m_color;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnColorRed();
+	afx_msg void OnColorGreen();
+	afx_msg void OnColorBlue();
+	afx_msg void OnUpdateColorRed(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateColorGreen(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateColorBlue(CCmdUI* pCmdUI);
+};
+
