152921712f39e47357fadc408e5a33b80881bd56
Kyungwon Chun <ruddyscent@gmail.com>
2021-02-12 13:53:55 +0900

    Add base codes for 06-11

---
+++ chapter06/06-11/resource.h
@@ -0,0 +1,21 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ColorSelect.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ColorSelectTYPE             130
+#define ID_COLOR_RED                    32771
+#define ID_COLOR_GREEN                  32772
+#define ID_COLOR_BLUE                   32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
